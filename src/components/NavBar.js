eb29535344e7e2d8236df47ae2466f09e2b93fa0
derrick1451 <haylederrick14@gmail.com>
2024-05-16 16:30:14 +0300

    Update navbar file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import humberger from '../assets/images/Menu.png'
+import logo from '../assets/images/angageicon.png'
 
 const NavBar = () => (
   <header>
-    <a className='logo' href='#'>Logo</a>
+    <a className='logo' href='#'><img src={logo} alt='logo'/></a>
     <img className='humberger' src={humberger} alt='humberger' />
   </header>
 );
