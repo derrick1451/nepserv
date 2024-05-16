2e730876444f4f9d62da3ccadf38566ca94e8632
derrick1451 <haylederrick14@gmail.com>
2024-05-16 11:56:00 +0300

    Update app file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from './components/NavBar';
 import HeroSection from './components/HeroSection';
+import ServiceSection from './components/ServiceSection';
 import React from 'react';
 import './App.css';
 
@@ -9,6 +10,7 @@ const App = ()=> {
     <>
       <NavBar/>
       <HeroSection/>
+      <ServiceSection/>
     </>
    
   )
