98138195e69e692f9c60ecb80a1a6593ee66c924
derrick1451 <haylederrick14@gmail.com>
2024-05-17 11:41:46 +0300

    Add react icons on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import humberger from '../assets/images/Menu.png'
 import logo from '../assets/images/angageicon.png'
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const NavBar = () => (
   <header>
     <a className='logo' href='#'><img src={logo} alt='logo'/></a>
-    <img className='humberger' src={humberger} alt='humberger' />
+    <RxHamburgerMenu className='humberger'/>
   </header>
 );
 
