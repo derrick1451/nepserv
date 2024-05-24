182a73df2e2bf8b061db6d3493d014e474b78299
derrick1451 <haylederrick14@gmail.com>
2024-05-24 10:40:09 +0300

    Modify navbar file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,17 +9,20 @@ const NavBar = () => {
   const toggleModal = () => {
     setModal(!modal);
   };
+  const closeModal = () => {
+    setModal(false);
+  };
  
   return (
     <header>
     <a className='logo' href='#'><img src={logo} alt='logo'/></a>
       <div className={`pop-up ${modal ? 'show' : ''}`}>
       <ul>
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#services'>Services</a></li>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#partners'>Partners</a></li>
-        <li><a href='#contact'>Contact</a></li>
+        <li><a href='#home' onClick={closeModal}>Home</a></li>
+        <li><a href='#services' onClick={closeModal}>Services</a></li>
+        <li><a href='#about' onClick={closeModal}>About</a></li>
+        <li><a href='#partners' onClick={closeModal}>Partners</a></li>
+        <li><a href='#contact' onClick={closeModal}>Contact</a></li>
       </ul>
       <IoMdClose onClick={toggleModal} className='close-icon'/>
 
