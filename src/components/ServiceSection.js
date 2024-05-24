24ac75f175705da4e2bc02e8160123b623c9a3a7
derrick1451 <haylederrick14@gmail.com>
2024-05-24 17:00:45 +0300

    Update services section section

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -1,78 +1,89 @@
-import restaurant from "../assets/images/image-restaurant.jpg";
-import plane from "../assets/images/image-plane.jpg";
-import currency from "../assets/images/image-currency.jpg";
-import confeti from "../assets/images/image-confetti.jpg";
-import ticket from "../assets/images/ticket.png";
-import project from "../assets/images/prop.jpg";
 import React from "react";
+import service from "../assets/images/task.svg";
+import element from "../assets/images/image-restaurant.jpg"
 const ServiceSection = () => {
   return (
-    <section id="services" className="s-section">
-      <div className="service-section">
-        <h2>Our Services</h2>
-        <div className="service-container">
-          <div className="items">
-            <div className="trial order-right">
-            <img src={restaurant} alt="restaurant" />
+    <section id="services" className="featured-section">
+      <div className="featured-container">
+        <div className="featured-text">
+          <h1>Our Services</h1>
+          <div className="feature-line"></div>
+        </div>
+        <div className="featured-work">
+          <div className="featured-img">
+            <img src={service} alt="featured image" />
+          </div>
+          <div className="featured-info">
+            <h2>Service Extension</h2>
+            <p>
+              Enhance support through WhatsApp BOT automations, Email
+              integrations, website integrations and phone call integrations to
+              deliver the best customer service to your clients.
+            </p>
+            <div className="featured-btn">
+              {" "}
+              <button>See More</button>
             </div>
-            <div className="order-left s-text">
-              <h3>Ticket Management</h3>
-              <p>
-                Manage all company and product related issues and tickets from
-                one central place.
-              </p>
-              <div className="s-btn">
-                <button>See More</button>
-              </div>
-            </div>
+          </div>
+        </div>
+
+        <div className="featured-cards">
+          <div className="f-card confetti">
+            <h3>Robust sales management</h3>
+            <p>
+              Better track prospects, targets, field activity through a fully
+              built sales management module
+            </p>
+            <button>See More</button>
           </div>
 
-          <div className="items">
-            <div className="trial">
-            <img src={plane} alt="restaurant" />
-            </div>
-            <div className="s-text">
-              <h3>Ticket Management</h3>
-              <p>
-                Manage all company and product related issues and tickets from
-                one central place.
-              </p>
-              <div className="s-btn">
-                <button>See More</button>
-              </div>
-            </div>
+          <div className="f-card element overlay">
+            <h3>Robust sales management</h3>
+            <p>
+              Better track prospects, targets, field activity through a fully
+              built sales management module
+            </p>
+            <button>See More</button>
           </div>
-          <div className="items">
-            <div className="trial order-right">
-            <img src={confeti} alt="restaurant" />
-            </div>
-            <div className="order-left s-text" >
-              <h3>Ticket Management</h3>
-              <p>
-                Manage all company and product related issues and tickets from
-                one central place.
-              </p>
-              <div className="s-btn">
-                <button>See More</button>
-              </div>
-            </div>
+
+          <div className="f-card drag overlay">
+            <h3>Ticket Management</h3>
+            <p>
+              Manage all company and product related issues and tickets from one
+              central place
+            </p>
+            <button>See More</button>
           </div>
-          <div className="items">
-            <div className="trial">
-            <img src={currency} alt="restaurant" />
-            </div>
-            <div className="s-text">
-              <h3>Ticket Management</h3>
-              <p>
-                Manage all company and product related issues and tickets from
-                one central place.
-              </p>
-              <div className="s-btn">
-                <button>See More</button>
-              </div>
-            </div>
+
+          <div className="f-card d-flex overlay">
+            <h3>Streamlined Data processing</h3>
+            <p>
+              Save time, money while improving efficiency of operations through
+              the enhanced data processing tool accessible from anywhere at any
+              time.
+            </p>
+            <button>See More</button>
           </div>
-      
+
+          <div className="f-card bg overlay">
+            <h3>Project Management & Product Development</h3>
+            <p>
+              Track the performance of all your projects, tasks and sub tasks
+              better so as to deliver projects against the best quality and time
+              expectations. Developed in compliance with ISO 9001:2015 & ISMS
+              27001 Information security acceptable standards.
+            </p>
+            <button>See More</button>
+          </div>
+
+          <div className="f-card confetti">
+            <h3>System Uptime monitoring</h3>
+            <p>
+              The easiest way to monitor system’s availability so as to serve
+              your customers and clients better.
+            </p>
+            <button>See More</button>
+          </div>
         </div>
       </div>
     </section>
